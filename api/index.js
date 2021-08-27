d9514aa98627b5bea5a1737f7565c8bd5eed2fa4
Simon Allan <stallan464@googlemail.com>
2021-08-27 17:16:30 +0100

    changed server message

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ const app = require('express');
 const Port = process.env.PORT || 3000;
 
 app.listen(Port, ()=>{
-    console.log(`server is listening at ${Port}`);
+    console.log(`Allll aboard! This server is departing from Port ${Port}! Choo choo!`);
 })
