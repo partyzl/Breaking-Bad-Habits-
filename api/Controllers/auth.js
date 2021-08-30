0fb3408884488d871fa54f388736b42d9d0e0ad2
Parth Patel <parth6796@yahoo.com>
2021-08-30 13:06:19 +0100

    added a register controller for auth

--- api/Controllers/auth.js
+++ api/Controllers/auth.js
@@ -9,9 +9,14 @@ const User = require('../Models/user')
 
 router.post('/register', async (req, res) => {
     try {
-        
+        const salt = await bcrypt.genSalt(); //creates the salt
+        const hashed = await bcrypt.hash(req.body.password, salt)//hashes and salts the pw
+        await User.create({...req.body, password: hashed})//calls create and adds to your db
+        res.status(201)
+        .json({message: 'User created!'})
     } catch (error) {
-        
+        res.status(500)
+        .json({error})
     }
 })
 
