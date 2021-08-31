902621b06a4cebd89e16ca50aee5507e951c12d8
KennethCWY <yeung.kenneth.c.w@gmail.com>
2021-08-31 11:50:53 +0100

    modify tables in 1_setup.sql

--- database/1_setup.sql
+++ database/1_setup.sql
@@ -1,14 +1,20 @@
 CREATE TABLE IF NOT EXISTS  users (
-    userId serial PRIMARY KEY,
-    userNames varchar(25) NOT NULL,
-    passwords varchar(50)
+    username varchar(25) NOT NULL UNIQUE,
+    password_digest varchar(500),
+    email varchar(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS habits (
-    userId int PRIMARY KEY, 
+    username varchar(25) NOT NULL,
+    habitId int serial,
     habit varchar(100) NOT NULL,
     selectedDays int[],
-    streak int, 
-    startDate date,
-    latestDate date
+    FOREIGN KEY(username) references users(username)
+);
+
+CREATE TABLE IF NOT EXISTS track (
+    habitId int,
+    streak int NOT NULL,
+    date date NOT NULL,
+    FOREIGN KEY(habitId) references habits(habitId)
 );
