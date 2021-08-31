d8a46ae35c96c31f08798b55effb697df7f5ad06
Parth Patel <parth6796@yahoo.com>
2021-08-31 12:20:19 +0100

    wrote get all habits controller

--- api/Controllers/habit.js
+++ api/Controllers/habit.js
@@ -4,3 +4,15 @@ const {verifyToken} = require('../middleware/auth')
 
 const User = require('../Models/user')
 const Habit = require('../Models/habit')
+
+//Read all habits for user
+router.get('/', verifyToken, async (req, res) => {
+    try {
+        const result = await Habit.sortByUserName(req.username);
+        res.status(200)
+        .json({result})
+    } catch (error) {
+        res.status
+        .send({error})
+    }
+})
