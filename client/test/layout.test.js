a5fc085dfbfb0c2c1c0c11e568a25cbafcaa3f78
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-08-31 16:17:16 +0100

    updated tests

--- client/test/layout.test.js
+++ client/test/layout.test.js
@@ -1,74 +1,28 @@
-  /**
+/**
  * @jest-environment jsdom
  */
-   const fs = require('fs');
-   const path = require('path');
-   const html = fs.readFileSync(path.resolve(__dirname, '/client/index.html'), 'utf8');
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(
+  path.resolve(__dirname, "/client/index.html"),
+  "utf8"
+);
 
+describe("index.html", () => {
+  beforeEach(() => {
+    document.documentElement.innerHTML = html.toString();
+  });
 
-   describe('index.html', () => {
-    beforeEach(() => {
-        document.documentElement.innerHTML = html.toString();
+  describe("head", () => {
+    test("it has a title", () => {
+      const head = document.querySelector("head");
+      expect(head).toBeTruthy();
+      expect(head.textContent).toContain("Habitual");
     });
 
-    describe('head', () => {
-        test('it has a title', () => {
-            const head = document.querySelector('head');
-            expect(head).toBeTruthy();
-            expect(head.textContent).toContain('Love Island Reacts');
-        });
-
-        test('there is a favicon', () => {
-            const head = document.querySelector('head');
-            expect(head.innerHTML).toContain('link rel="icon"');
-        });
-
-        test('there is a css stylesheet', () => {
-            const head = document.querySelector('head');
-            expect(head.innerHTML).toContain('link rel="stylesheet"');
-        });
-
-        describe('body', () => {
-            test('it has a header', () => {
-                let header = document.querySelector('header');
-                expect('header').toBeTruthy();
-                expect(header.textContent).toContain('Love Island Reacts');
-            });
-            test('it has a section to display posts', () => {
-                expect(document.querySelector('#posts__container')).toBeTruthy;
-            });
-            test('it has a section to display replies', () => {
-                expect(document.querySelector('#reply__container')).toBeTruthy;
-            });
-
-            describe('Post Form', () => {
-                test('it exists', () => {
-                    let postForm = document.querySelector('.post__actions');
-                    expect(postForm).toBeTruthy();
-                });
-
-                test('it has a button for submitting a post', () => {
-                    let postBtn = document.querySelector('.post__btn');
-                    expect(postBtn).toBeTruthy();
-                    expect(postBtn.getAttribute('type')).toBe('submit');
-                });
-            });
-
-           
-
-            describe('footer', () => {
-                test('it exists', () => {
-                    let footer = document.querySelector('footer');
-                    expect(footer).toBeTruthy();
-                });
-
-                test('it contains the names of contributors', () => {
-                    let footer = document.querySelector('footer');
-                    expect(footer.textContent).toContain(
-                        '©Elicia, Tobi, Corneliu & Abi'
-                    );
-                });
-            });
-        });
+    test("there is a css stylesheet", () => {
+      const head = document.querySelector("head");
+      expect(head.innerHTML).toContain('link rel="stylesheet"');
     });
+  });
 });
