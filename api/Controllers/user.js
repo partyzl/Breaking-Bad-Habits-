cc7014ba353ffeb247d1315ae40900311d9d79d7
Parth Patel <parth6796@yahoo.com>
2021-08-31 12:02:33 +0100

    added habit route via username

--- api/Controllers/user.js
+++ api/Controllers/user.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 const {verifyToken} = require('../middleware/auth')
 const User = require('../Models/user')
 
+//habit route via user
+const habitRoutes = require('./habit')
+router.use('/:username/habits', habitRoutes)
+//habit route via user
+
 router.get('/:username', verifyToken, async (req, res) => {
     try {
         const user = await User.findByUserName(req.params.username);
