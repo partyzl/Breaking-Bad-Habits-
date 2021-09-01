448ad43275918a45e6cae0d3e35ec96e53a5f65a
Parth Patel <parth6796@yahoo.com>
2021-09-01 10:23:43 +0100

    added test seed file

--- api/tests/integration/test_seeds.sql
+++ api/tests/integration/test_seeds.sql
@@ -1,6 +1,20 @@
-TRUNCATE users RESTART IDENTITY;
+TRUNCATE users, habits, track RESTART IDENTITY;
 
 INSERT INTO users (username, email, password_digest)
 VALUES
 ('Testy', 'test@testy.com', 'password'),
 ('Testo', 'test@testo.com', 'wordword');
+
+INSERT INTO habits (username, habit, selectedDays)
+VALUES  
+
+    ('Testy', 'running a 5k', 6),
+    ('Testy', 'going to the gym', [2,4,6]),
+    ('Testo', 'waking up at 5', [1,2,3,4,5]);
+
+INSERT INTO track (habitId, streak, date)
+VALUES
+    
+    (1, 17, '01 Sep 2021'),
+    (2, 12, '31 Aug 2021'),
+    (3, 40, '01 Sep 2021');
