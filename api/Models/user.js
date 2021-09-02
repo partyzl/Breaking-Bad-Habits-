6dfac30352d7e9f5ef985811556ece8d726c8676
Parth Patel <parth6796@yahoo.com>
2021-09-02 11:52:51 +0100

    tried using standard query syntax instead of string literals

--- api/Models/user.js
+++ api/Models/user.js
@@ -20,12 +20,12 @@ class User {
     });
   }
 
-  static create({ username, email, password }) {
+  static create( username, email, password ) {
     return new Promise(async (res, rej) => {
       try {
         let result =
-          await db.query(SQL`INSERT INTO users (username, email, password_digest)
-                                        VALUES (${username}, ${email}, ${password});`);
+          await db.query(`INSERT INTO users (username, email, password_digest)
+                                        VALUES ($1, $2, $3) RETURNING *;`[username, email, password]);
         let newUser = new User(result.rows[0]);
         res(newUser);
       } catch (error) {
@@ -34,11 +34,11 @@ class User {
     });
   }
 
-  static findByUserName(username) {
+  static findByUserName(email) {
     return new Promise(async (res, rej) => {
       try {
         let result = await db.query(
-          SQL`SELECT * FROM users WHERE username = ${username};`
+        `SELECT * FROM users WHERE email = $1;`[email]
         );
         let user = new User(result.rows[0]);
         res(user);
